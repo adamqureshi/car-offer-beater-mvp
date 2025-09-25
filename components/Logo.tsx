8e99c7c2d11c0ade58957a7d8bc1415ea705bf47
Adam Qureshi <adamqureshi@gmail.com>
2025-09-25 10:26:51 -0400

    Update Logo.tsx

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,8 +1,9 @@
 export default function Logo() {
   return (
-    <div className="flex items-center gap-2">
-      <div className="h-8 w-8 rounded-xl bg-emerald-600" />
-      <span className="text-xl font-bold">OfferBeater</span>
+    <div className="flex items-center gap-3">
+      <div className="h-8 w-8 rounded-xl bg-brandBlue" />
+      <span className="text-xl font-extrabold tracking-tight">OfferBeater</span>
     </div>
   );
 }
+
